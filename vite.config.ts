dcf962c832964dcaa327e9d413ad287c2762da10
Sharath Kumar <sharathkr@protonmail.com>
2023-10-26 14:36:04 +0530

    update: add `@` alias - install deps - update tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(fileURLToPath(new URL('./src', import.meta.url))),
+      },
+    ],
+  },
 });
