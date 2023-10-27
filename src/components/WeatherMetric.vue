2945c6af9edd0028245d5e3619414205fc4341b3
Sharath Kumar <sharathkr@protonmail.com>
2023-10-27 12:29:10 +0530

    fix: run format

--- src/components/WeatherMetric.vue
+++ src/components/WeatherMetric.vue
@@ -4,7 +4,9 @@
       class="flex flex-shrink-0 items-center justify-center p-4 border border-gray-400 rounded-md">
       <slot name="icon"></slot>
     </div>
-    <div class="flex flex-col gap-2 items-start justify-center" v-if="$slots.metric || $slots.meta">
+    <div
+      class="flex flex-col gap-2 items-start justify-center"
+      v-if="$slots.metric || $slots.meta">
       <p class="text-4xl sm:text-6xl text-zinc-700 relative font-secondary">
         <slot name="metric"></slot>
       </p>
