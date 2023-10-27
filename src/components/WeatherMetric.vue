a31b1466f2cf057b703c93189581991ae6e73a6f
Sharath Kumar <sharathkr@protonmail.com>
2023-10-27 11:32:17 +0530

    update: add metric component

---
+++ src/components/WeatherMetric.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex gap-4">
+    <div
+      class="flex flex-shrink-0 items-center justify-center p-4 border border-gray-400 rounded-md">
+      <slot name="icon"></slot>
+    </div>
+    <div class="flex flex-col gap-2 items-start justify-center" v-if="$slots.metric || $slots.meta">
+      <p class="text-4xl sm:text-6xl text-zinc-700 relative font-secondary">
+        <slot name="metric"></slot>
+      </p>
+      <div class="flex items-center gap-4 flex-wrap">
+        <slot name="meta"></slot>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/WeatherWidget.vue
+++ src/components/WeatherWidget.vue
@@ -7,7 +7,7 @@
         <icon icon="solar:map-point-broken" />
         <p>
           <strong class="text-zinc-800 font-semibold">{{ weather.location.name }}</strong>
-          <span class="ml-2 text-sm font-normal text-zinc-400">{{
+          <span class="ml-2 text-sm font-normal text-zinc-500">{{
             weather.location.country
           }}</span>
         </p>
@@ -17,7 +17,7 @@
     <div
       class="flex items-center justify-center sm:justify-start flex-wrap gap-4 sm:gap-8 py-4 sm:py-8">
       <div
-        class="flex flex-shrink-0 items-center justify-center p-4 border border-gray-200 rounded-sm">
+        class="flex flex-shrink-0 items-center justify-center p-4 border border-gray-400 rounded-md">
         <img
           class="w-20 sm:w-32 h-20 sm:h-32 object-contain"
           :src="weather.current.condition.icon"
@@ -31,74 +31,72 @@
             >&deg; C</span
           >
         </p>
-        <p class="text-xl font-normal text-zinc-300 sm:ml-2">
+        <p class="text-xl font-normal text-zinc-800 sm:ml-2">
           {{ weather.current.condition.text }}
         </p>
       </div>
     </div>
 
     <div class="flex flex-wrap justify-between gap-4">
-      <div class="flex gap-4">
-        <div
-          class="flex flex-shrink-0 items-center justify-center p-4 border border-gray-200 rounded-sm">
+      <weather-metric>
+        <template #icon>
           <icon icon="solar:wind-broken" height="48" />
-        </div>
-        <div class="flex flex-col gap-2 items-start">
+        </template>
+        <template #metric>
           <p class="text-4xl sm:text-6xl text-zinc-700 relative font-secondary">
             {{ weather.current.wind_kph }}
             <span class="text-lg text-zinc-400">km/h</span>
           </p>
-          <div class="flex items-center gap-4 flex-wrap">
-            <p class="text-xs text-zinc-400 inline-flex gap-1 items-center">
-              <icon icon="solar:compass-broken" height="16" />
-              <span>{{ weather.current.wind_dir }}</span>
-            </p>
-            <p class="text-xs text-zinc-400 inline-flex gap-1 items-center">
-              <icon icon="wi:barometer" height="16" />
-              <span>{{ weather.current.wind_degree }} &deg;</span>
-            </p>
-          </div>
-        </div>
-      </div>
+        </template>
 
-      <div class="flex gap-4">
-        <div
-          class="flex flex-shrink-0 items-center justify-center p-4 border border-gray-200 rounded-sm">
+        <template #meta>
+          <p class="text-xs text-zinc-400 inline-flex gap-1 items-center">
+            <icon icon="solar:compass-broken" height="16" />
+            <span>{{ weather.current.wind_dir }}</span>
+          </p>
+          <p class="text-xs text-zinc-400 inline-flex gap-1 items-center">
+            <icon icon="wi:barometer" height="16" />
+            <span>{{ weather.current.wind_degree }} &deg;</span>
+          </p>
+        </template>
+      </weather-metric>
+
+      <weather-metric>
+        <template #icon>
           <icon icon="carbon:humidity-alt" height="48" />
-        </div>
-        <div class="flex flex-col gap-2 items-start">
+        </template>
+
+        <template #metric>
           <p class="text-4xl sm:text-6xl text-zinc-700 relative font-secondary">
             {{ weather.current.humidity }}
             <span class="text-lg text-zinc-400">%</span>
           </p>
-        </div>
-      </div>
+        </template>
+      </weather-metric>
 
-      <div class="flex gap-4">
-        <div
-          class="flex flex-shrink-0 items-center justify-center p-4 border border-gray-200 rounded-sm">
+      <weather-metric>
+        <template #icon>
           <icon icon="solar:cloud-bolt-minimalistic-broken" height="48" />
-        </div>
-        <div class="flex flex-col gap-2 items-start">
+        </template>
+        <template #metric>
           <p class="text-4xl sm:text-6xl text-zinc-700 relative font-secondary">
             {{ weather.current.precip_mm }}
             <span class="text-lg text-zinc-400">mm</span>
           </p>
-        </div>
-      </div>
+        </template>
+      </weather-metric>
 
-      <div class="flex gap-4">
-        <div
-          class="flex flex-shrink-0 items-center justify-center p-4 border border-gray-200 rounded-sm">
+      <weather-metric>
+        <template #icon>
           <icon icon="pajamas:tachometer" height="48" />
-        </div>
-        <div class="flex flex-col gap-2 items-start">
+        </template>
+        <template #metric>
           <p class="text-4xl sm:text-6xl text-zinc-700 relative font-secondary">
             {{ weather.current.pressure_in }}
             <span class="text-lg text-zinc-400">in</span>
           </p>
-        </div>
-      </div>
+        </template>
+      </weather-metric>
     </div>
 
     <a
@@ -114,6 +112,7 @@
 </template>
 
 <script setup lang="ts">
+  import WeatherMetric from '@/components/WeatherMetric.vue';
   import { Response } from '@/composables/useWeatherApi';
 
   interface Props {
